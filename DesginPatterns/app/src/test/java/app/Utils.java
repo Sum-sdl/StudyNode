181d293cd3d36e369aa4a1902ca644b6607cac7b
shidelei <shidelei@vip.house365.com>
2017-06-12 15:00:17 +0800

    设计模式

---
+++ DesginPatterns/app/src/test/java/app/Utils.java
@@ -0,0 +1,13 @@
+package app;
+
+/**
+ * Created by sdl on 2017/6/12.
+ */
+
+public class Utils {
+
+    public static void systemPrints(Object obj) {
+        System.out.print(obj);
+    }
+
+}
---
+++ DesginPatterns/app/src/test/java/design_patterns/Design.java
@@ -0,0 +1,18 @@
+package design_patterns;
+
+import org.junit.Test;
+
+import app.Utils;
+
+/**
+ * Created by sdl on 2017/6/12.
+ */
+
+public class Design {
+
+    @Test
+    public void design_func() throws Exception {
+        Utils.systemPrints("func start");
+    }
+
+}
